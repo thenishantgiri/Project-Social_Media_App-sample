6e4f3a97aeb1055eca0a0576d106326ea20f450b
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-06-30 23:02:43 +0530

    users : controller made

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const { db } = require("./db/models");
 const app = express();
 
 // we're first making connection with database, then starting the server (promise syntax)
-db.sync({ alter: true })
+db.sync({ force: true })
   .then(() => {
     app.listen(8383, () => {
       console.log("server has started on http://localhost:8383");
