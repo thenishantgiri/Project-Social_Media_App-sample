4de3607fc0198143ed110b8eb02cadf4a21b0b61
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-06-30 20:45:52 +0530

    Database definitions updated

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+// requiring database from database models
+const { db } = require("./db/models");
+
+const app = express();
+
+// we're first making connection with database, then starting the server (promise syntax)
+db.sync({ alter: true })
+  .then(() => {
+    app.listen(8383, () => {
+      console.log("server has started on http://localhost:8383");
+    });
+  })
+  .catch((err) => {
+    console.error(new Error("could not start database"));
+    console.error(err);
+  });
